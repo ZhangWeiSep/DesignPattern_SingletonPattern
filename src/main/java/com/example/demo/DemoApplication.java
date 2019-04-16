06990224d070d03b71c1eb96eefe08e7f644ca90
ZhangWei <zhangweisep@aliyun.com>
2019-04-16 20:57:26 +0800

    demo补全

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,8 +6,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }*/
+
+    public static void main(String[] args) {
+        //定义5个大臣
+        int ministerNum =5;
+        for(int i=0;i<ministerNum;i++){
+            Emperor emperor = Emperor.getInstance();
+            System.out.print("第"+(i+1)+"个大臣参拜的是：");
+            emperor.say();
+        }
     }
 
 }
